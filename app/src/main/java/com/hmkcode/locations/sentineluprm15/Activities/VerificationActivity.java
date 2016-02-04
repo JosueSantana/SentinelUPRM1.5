148fd393224298baa52fa479f6a71461e39ea7e5
Josue Santana <josue.santana1@upr.edu>
2016-02-04 00:22:48 -0400

    Added visual interface to the sign up screens

--- app/src/main/java/com/hmkcode/locations/sentineluprm15/Activities/VerificationActivity.java
+++ app/src/main/java/com/hmkcode/locations/sentineluprm15/Activities/VerificationActivity.java
@@ -1,9 +1,20 @@
 package com.hmkcode.locations.sentineluprm15.Activities;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import com.hmkcode.locations.sentineluprm15.R;
 
 /**
  * Created by a136803 on 2/3/16.
  */
 public class VerificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verification);
+    }
+
 }
+
