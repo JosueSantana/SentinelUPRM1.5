5177f00ddf5a7595ca84951bac445a4cfe618e93
Jean <jean.mendez2@upr.edu>
2016-03-26 23:22:36 -0400

    Added Report a Problem and Send Feedback Capabilities.

--- app/src/main/java/Fragments/FeedbackFragment.java
+++ app/src/main/java/Fragments/FeedbackFragment.java
@@ -1,13 +1,26 @@
 package Fragments;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+
+import org.cryptonode.jncryptor.CryptorException;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import OtherHandlers.CryptographyHandler;
+import OtherHandlers.JSONHandler;
+import OtherHandlers.ValuesCollection;
 import edu.uprm.Sentinel.R;
 
 public class FeedbackFragment extends Fragment {
@@ -16,11 +29,13 @@ public class FeedbackFragment extends Fragment {
     private static final String ARG_PARAM1 = "title";
     private static final String ARG_PARAM2 = "hint";
     private static final String ARG_PARAM3 = "footer";
+    private static final String ARG_PARAM4 = "reportProblem";
 
     // TODO: Rename and change types of parameters
     private String title;
     private String hint;
     private String footer;
+    private boolean isReportProblem;
 
     public FeedbackFragment() {
         // Required empty public constructor
@@ -52,6 +67,7 @@ public class FeedbackFragment extends Fragment {
             title = getArguments().getString(ARG_PARAM1);
             hint = getArguments().getString(ARG_PARAM2);
             footer = getArguments().getString(ARG_PARAM3);
+            isReportProblem = getArguments().getBoolean(ARG_PARAM4);
 
             System.out.println("print title: " + title);
         }
@@ -64,8 +80,116 @@ public class FeedbackFragment extends Fragment {
         View root =  inflater.inflate(R.layout.fragment_feedback, container, false);
 
         TextView titleView = (TextView) root.findViewById(R.id.feedbacktitle);
-        AutoCompleteTextView hintView = (AutoCompleteTextView) root.findViewById(R.id.myFeedback);
+        final AutoCompleteTextView hintView = (AutoCompleteTextView) root.findViewById(R.id.myFeedback);
         TextView footerView = (TextView) root.findViewById(R.id.feedbackmessage);
+        Button submitButton = (Button) root.findViewById(R.id.button);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("lollll");
+
+                final CryptographyHandler crypto;
+                try {
+                    crypto = new CryptographyHandler();
+
+                    Runnable r = new Runnable(){
+                        @Override
+                        public void run() {
+                            try{
+                                String URL = isReportProblem ? ValuesCollection.REPORT_PROBLEM_URL : ValuesCollection.SEND_FEEDBACK_URL;
+
+                                JSONObject registerJSON = new JSONObject();
+                                registerJSON.put("message", hintView.getText());
+                                registerJSON.put("os", ValuesCollection.ANDROID_OS_STRING);
+                                registerJSON.put("token", getToken());
+
+                                Ion.with(getContext())
+                                        .load("PUT", URL)
+                                        .setBodyParameter(ValuesCollection.SENTINEL_MESSAGE_KEY, crypto.encryptJSON(registerJSON))
+                                        .asString()
+                                        .setCallback(new FutureCallback<String>() {
+                                            @Override
+                                            public void onCompleted(Exception e, String receivedJSON) {
+                                                // Successful Request
+                                                if (requestIsSuccessful(e)) {
+                                                    JSONObject decryptedValue = getDecryptedValue(receivedJSON);
+                                                    System.out.println(decryptedValue);
+
+                                                    // Received Success Message
+                                                    if (receivedSuccessMessage(decryptedValue)) {
+
+                                                    }
+
+                                                    // Message Was Not Successful.
+                                                    else {
+
+                                                    }
+
+
+                                                }
+                                                // Errors
+                                                else {
+
+                                                }
+                                            }
+
+                                            // Extract Success Message From Received JSON.
+                                            private boolean receivedSuccessMessage(JSONObject decryptedValue) {
+                                                String success = null;
+                                                try {
+                                                    success = decryptedValue.getString("success");
+                                                    return success.equals("1");
+                                                } catch (JSONException e) {
+                                                    e.printStackTrace();
+                                                }
+                                                return false;
+                                            }
+
+                                            // Verify if there was an Error in the Request.
+                                            private boolean requestIsSuccessful(Exception e) {
+                                                return e == null;
+                                            }
+
+                                            // Convert received JSON String into a Decrypted JSON.
+                                            private JSONObject getDecryptedValue(String receivedJSONString) {
+                                                try {
+                                                    JSONObject receivedJSON = JSONHandler.convertStringToJSON(receivedJSONString);
+                                                    String encryptedStringValue = JSONHandler.getSentinelMessage(receivedJSON);
+                                                    String decryptedStringValue = crypto.decryptString(encryptedStringValue);
+                                                    JSONObject decryptedJSON = JSONHandler.convertStringToJSON(decryptedStringValue);
+                                                    return decryptedJSON;
+                                                } catch (JSONException e) {
+                                                    e.printStackTrace();
+                                                } catch (CryptorException e) {
+                                                    e.printStackTrace();
+                                                }
+                                                return null;
+                                            }
+                                        });
+                            } catch (CryptorException e) {
+                                e.printStackTrace();
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        private String getToken() {
+                            SharedPreferences credentials = getActivity().getSharedPreferences(ValuesCollection.CREDENTIALS_SP, 0);
+                            String storedToken = credentials.getString(ValuesCollection.TOKEN_KEY, null);
+                            return storedToken;
+                        }
+                    };
+
+                    new Thread(r).start();
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
 
         titleView.setText(title);
         hintView.setHint(hint);
